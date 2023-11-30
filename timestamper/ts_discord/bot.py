c031d0f32af2ffb347e660446c94d987ed982cb2
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-11-30 17:39:50 -0500

    Add $set_utc_offset, tz persist & adjustment in replies

--- timestamper/ts_discord/bot.py
+++ timestamper/ts_discord/bot.py
@@ -1,9 +1,12 @@
 # This example requires the 'message_content' intent.
+import datetime
+import json
 import os
 import time
 
 import discord
 import parsedatetime
+from discord.abc import Snowflake
 
 from timestamper.main import try_parsedatetime
 
@@ -13,10 +16,52 @@ intents.message_content = True
 if __name__ == '__main__':
     client = discord.Client(intents=intents)
 
+_start_time = datetime.datetime.now().timestamp()
+system_utc_offset = (
+        datetime.datetime.utcfromtimestamp(_start_time) -
+        datetime.datetime.fromtimestamp(_start_time)
+).total_seconds()
+
+users_tz_path = "./user_timezoneoffsets.json"
+should_init = os.path.getsize(users_tz_path) == 0
+if should_init:
+    with open(users_tz_path, mode='w+') as f:
+        f.write('{}')
+        f.flush()
+        f.close()
+
+with open(users_tz_path, mode='r') as f:
+    user_timezones: dict[str, float] = json.load(f)
+    print(json.dumps(user_timezones))
+
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
+
+one_minute_secs = 60
+one_hour_secs = 60 * one_minute_secs
+
+
+def adjust_time_for_user(t: float, user_id: Snowflake):
+    user_tz_offset_hrs = user_timezones[str(user_id)]
+    if user_tz_offset_hrs is None:
+        return t
+    print(t, system_utc_offset, user_tz_offset_hrs)
+    return t - (system_utc_offset + one_hour_secs * user_tz_offset_hrs)
+
+
+def set_user_tz_offset(user_id: int, tz_offset: float):
+    user_timezones[str(user_id)] = tz_offset
+    with open("./user_timezoneoffsets.json", mode='w') as f:
+        json.dump(user_timezones, f)
+        f.flush()
+        f.close()
+
+
+def get_user_tz_offset(user_id: int):
+    user_timezones.get(str(user_id))
 
 
 @client.event
@@ -24,10 +69,20 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    content: str = message.content
+
+    if content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-    content: str = message.content
+    if content.startswith('$set_utc_offset'):
+        tz_to_parse = content[len('$set_utc_offset'):].strip()
+        try:
+            tz_offset = float(tz_to_parse)
+            set_user_tz_offset(message.author.id, tz_offset)
+            await message.channel.send(f"Set your time zone offset to '{tz_offset}'.")
+        except:
+            await message.channel.send(f'Failed to parse "tz_offset" to a time zone offset')
+
     if '$time' in content:
         res = try_parsedatetime(content[len('$time'):])
         st: time.struct_time = res[0]
@@ -35,7 +90,7 @@ async def on_message(message):
         if status.hasDateOrTime:
             # time_str = time.strftime('%Y-%m-%dT%H:%M:%SZ', timestamp)
             # t = datetime(*timestamp[:6])
-            epoch_time = time.mktime(st)
+            epoch_time = adjust_time_for_user(time.mktime(st), message.author.id)
             await message.channel.send(
                 "Time: <t:{:.0f}:F> (<t:{:.0f}:R>)".format(epoch_time, epoch_time))
         else:
